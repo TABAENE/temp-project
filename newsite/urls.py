0927a7161dbeb65e0ac83699e01e7e0d02737046
haque <tabaene.haque786@gmail.com>
2017-03-26 15:21:49 +0530

    trigger first build.

--- newsite/urls.py
+++ newsite/urls.py
@@ -1,7 +1,7 @@
 from django.conf.urls import include, url
 from django.contrib import admin
 from home.views import Home
-Trigger my build
+Trigger  build
 urlpatterns = [
     url(r'^admin/', include(admin.site.urls)),
     url(r'^$', Home.as_view()),
